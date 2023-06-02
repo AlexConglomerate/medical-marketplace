1f55738a192c980b505a328be9131ce0a73c1a93
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2023-06-02 15:40:41 +0600

    Made input manageable

--- src/ui/input.jsx
+++ src/ui/input.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Input = () => {
+    const [text, setText] = useState('');
+    const handleChange = (e) => {
+        const {value} = e.target
+        setText(value)
+    }
     return (
         <input
+            onChange={(e)=>handleChange(e)}
             className="w-72 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             type="text"
-            placeholder="Введите текст"
+            placeholder="Search"
+            value={text}
         />
     );
 };
