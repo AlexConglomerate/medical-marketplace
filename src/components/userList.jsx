e2f46f25db76cf2d006815eabbb059f512e717ed
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2023-06-02 16:15:02 +0600

    Added the ability to sort by name

--- src/components/userList.jsx
+++ src/components/userList.jsx
@@ -3,6 +3,9 @@ import axios from 'axios';
 import Card from "./card";
 import {useSelector} from "react-redux";
 import {getUser} from "../store/user";
+import Button from "../ui/button";
+import _ from "lodash";
+
 
 const UserList = () => {
     const [users, setUsers] = useState([]);
@@ -12,8 +15,16 @@ const UserList = () => {
         setUsers(newUsers.users);
     }, [newUsers]);
 
+    const handleSort = () => {
+        const _users = [...users]
+        const sortedUsers = _.orderBy(_users, ["name.first"], ["asc"])
+        setUsers(sortedUsers)
+
+    }
+
     return (
         <div className={'flex flex-row flex-wrap gap-10 my-24 '}>
+            <Button name={'Sort'} onClick={handleSort}/>
             {users && users.map(user => (
                 <Card user={user}/>
             ))}
